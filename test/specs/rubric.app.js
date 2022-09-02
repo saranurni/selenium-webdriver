ef0f89c56b071f93a30f73c163900bbfa5da1ca1
Sara Nurestani <saranurestani@gmail.com>
2022-09-02 23:44:41 +0100

    Assert- add expect title toHaveText

--- test/specs/rubric.app.js
+++ test/specs/rubric.app.js
@@ -3,5 +3,7 @@ describe("Rubric App", function () {
     await browser.url("https://inspiring-wisp-f3b8d3.netlify.app/");
 
     const title = $("h1");
+
+    await expect(title).toHaveText("Rubric App");
   });
 });
